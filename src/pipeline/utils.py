359fc66ab373acfa854a82e6f3d6687fc7c02698
Tejesh0209 <84062794+Tejesh0209@users.noreply.github.com>
2025-02-10 12:35:10 -0500

    app created

--- src/pipeline/utils.py
+++ src/pipeline/utils.py
@@ -54,3 +54,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
     except Exception as e:
         raise CustomException(e, sys)
     
+
+import os
+def load_object(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File {file_path} not found.")
+    with open(file_path, "rb") as file_obj:
+        return dill.load(file_obj)
